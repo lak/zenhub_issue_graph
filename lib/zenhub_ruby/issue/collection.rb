fb36157524de358083cf4b85d5b57346e4a4c8df
Luke Kanies <luke@clickety.app>
2020-05-08 15:57:37 -0700

    checkpoint
    Signed-off-by: Luke Kanies <luke@clickety.app>

---
+++ lib/zenhub_ruby/issue/collection.rb
@@ -0,0 +1,109 @@
+require 'ruby-graphviz'
+
+class ZenhubRuby::Issue::Collection
+  attr_reader :graph, :repo_name, :repo, :gh_client
+
+  def <<(issue)
+    @issues[issue.name] = issue
+  end
+
+  def [](name)
+    @issues[name]
+  end
+
+  def add(issue)
+    @issues[issue.name] = issue
+    @graph.add_nodes(issue.name)
+  end
+
+  def add_edge(left, right)
+    left.blocked_by(right)
+    right.blocking(left)
+    @graph.add_edges(left.name, right.name)
+  end
+
+  # Given a hash of data, create a new issue and add it to the collection,
+  # or find an existing issue. Return the result.
+  def add_or_find(hash)
+    issue = ZenhubRuby::Issue.new(hash)
+
+    if i = self[issue.name]
+      return i
+    else
+      # XXX We should load issue data from github
+      issues.add(issue)
+      return issue
+    end
+  end
+
+  def each
+    @issues.each { |name, issue| yield(name, issue) }
+  end
+
+  def initialize(name, gh_client)
+    @repo_name = name
+
+    @gh_client = gh_client
+    @issues = {}
+
+    @graph = GraphViz.new(:G, :type => :digraph, rankdir: "LR")
+
+    @indent = "  "
+  end
+
+  # Pre-load all of the issues from the github repo. We'll then layer this
+  # with dependency and epic data from zenhub
+  def load_issues
+    load_repository_data()
+
+    gh_client.list_issues(repo_name, { state: "all" }).each do |data|
+      puts data["title"]
+
+      issue = ZenhubRuby::Issue.new
+      issue.load_from_gh_data(data)
+      issue[:repo_id] = repo[:id]
+      self.add(issue)
+    end
+  end
+
+  def load_repository_data
+    @repo = gh_client.repo(repo_name)
+    @repo_id = repo[:id]
+  end
+
+  def print_dependencies(issue, level)
+    if level > 20
+      raise "You screwed up"
+    end
+
+    epic_info = ""
+    if issue.is_epic?
+      epic_info = " *"
+    end
+
+    # We should probably print the issue name and URL next
+    puts "#{@indent * level}#{issue}#{epic_info}"
+    #indent = "#{@indent * level}#{issue}"
+
+    # We need the github API to get the repo name. And to get the issue title.
+    #url = "https://github.com/"
+
+    #string = "%-20s %2s %-30s %s" % [indent, epic_info, 
+    issue.blockers.each do |blocker|
+      print_dependencies(blocker, level + 1)
+    end
+  end
+
+  def print_tree
+    level = 0
+
+    tree_tops.each do |issue|
+      print_dependencies(issue, level)
+    end
+  end
+
+  # Find all unblocked issues
+  def tree_tops
+    @issues.values.find_all { |issue| issue.non_blocking? }
+  end
+end
